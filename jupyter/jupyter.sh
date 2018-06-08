6b641ffeeb9abfcf7574aaffd3c2842f17b56abc
stone0117 <lookup010101@qq.com>
2018-06-08 11:35:12 +0800

    jupyter

---
+++ jupyter/jupyter.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd $HOME/shared
+#python -m IPython notebook
+jupyter notebook --config ~/.jupyter/snconfig.py #我自己用的, ip只能接收localhost
---
+++ jupyter/jupyter_other.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd $HOME/shared
+#python -m IPython notebook
+jupyter notebook --config ~/.jupyter/jupyter_notebook_config.py #能接收所有ip
